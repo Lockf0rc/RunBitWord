ef0e750acc6e5851b2263736b7f3ff0a452ee6e1
miguel Escamilla <metuxxman@gmail.com>
2016-01-19 20:11:27 -0800

    interface for data repersentation

---
+++ app/lib/Interf/IRender.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Miguel
+ * Date: 1/19/2016
+ * Time: 7:42 PM
+ */
+
+namespace Lockf0rc\Bitwords;
+
+
+interface IRender
+{
+    public function display();
+}
