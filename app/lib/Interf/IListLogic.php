e34d5434adc57f4be2ca4b28af88af2a270e925d
miguel Escamilla <metuxxman@gmail.com>
2016-01-19 20:11:00 -0800

    When implemented Set Requirement of Unique Choices.

---
+++ app/lib/Interf/IListLogic.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Miguel
+ * Date: 1/19/2016
+ * Time: 6:24 PM
+ */
+
+namespace Lockf0rc\Bitwords;
+
+
+Abstract class IListLogic
+{
+    protected $ListArray;
+
+    public abstract function isUnique();
+
+
+}
